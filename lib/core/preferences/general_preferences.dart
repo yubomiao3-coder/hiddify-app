de359d1b6eaa95b752d9246bfa83fe4859fc48c1
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:15:22 +0800

    rebrand: cloud-VPN (24/220)

--- lib/core/preferences/general_preferences.dart
+++ lib/core/preferences/general_preferences.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:hiddify/core/app_info/app_info_provider.dart';
-import 'package:hiddify/core/model/environment.dart';
-import 'package:hiddify/core/model/region.dart';
-import 'package:hiddify/core/preferences/actions_at_closing.dart';
+import 'package:cloud_vpn/core/app_info/app_info_provider.dart';
+import 'package:cloud_vpn/core/model/environment.dart';
+import 'package:cloud_vpn/core/model/region.dart';
+import 'package:cloud_vpn/core/preferences/actions_at_closing.dart';
 
-import 'package:hiddify/core/preferences/preferences_provider.dart';
-import 'package:hiddify/core/utils/preferences_utils.dart';
-import 'package:hiddify/features/per_app_proxy/model/per_app_proxy_mode.dart';
-import 'package:hiddify/features/window/notifier/window_notifier.dart';
-import 'package:hiddify/utils/platform_utils.dart';
+import 'package:cloud_vpn/core/preferences/preferences_provider.dart';
+import 'package:cloud_vpn/core/utils/preferences_utils.dart';
+import 'package:cloud_vpn/features/per_app_proxy/model/per_app_proxy_mode.dart';
+import 'package:cloud_vpn/features/window/notifier/window_notifier.dart';
+import 'package:cloud_vpn/utils/platform_utils.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'general_preferences.g.dart';
