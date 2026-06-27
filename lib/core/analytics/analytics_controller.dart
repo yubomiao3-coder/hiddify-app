69fbbea2dbc681044d5c40c8c722315e09e9d965
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:11:01 +0800

    rebrand: cloud-VPN (3/220)

--- lib/core/analytics/analytics_controller.dart
+++ lib/core/analytics/analytics_controller.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
-import 'package:hiddify/core/analytics/analytics_filter.dart';
-import 'package:hiddify/core/analytics/analytics_logger.dart';
+import 'package:cloud_vpn/core/analytics/analytics_filter.dart';
+import 'package:cloud_vpn/core/analytics/analytics_logger.dart';
 
-import 'package:hiddify/core/logger/logger_controller.dart';
-import 'package:hiddify/core/model/environment.dart';
-import 'package:hiddify/core/preferences/preferences_provider.dart';
-import 'package:hiddify/utils/custom_loggers.dart';
+import 'package:cloud_vpn/core/logger/logger_controller.dart';
+import 'package:cloud_vpn/core/model/environment.dart';
+import 'package:cloud_vpn/core/preferences/preferences_provider.dart';
+import 'package:cloud_vpn/utils/custom_loggers.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
