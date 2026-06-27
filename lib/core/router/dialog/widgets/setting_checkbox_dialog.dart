84e7d94949e9c5aead66eb6627bb207aec522cdc
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:20:09 +0800

    rebrand: cloud-VPN (47/220)

--- lib/core/router/dialog/widgets/setting_checkbox_dialog.dart
+++ lib/core/router/dialog/widgets/setting_checkbox_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/features/route_rules/notifier/rule_notifier.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/features/route_rules/notifier/rule_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:protobuf/protobuf.dart';
 
