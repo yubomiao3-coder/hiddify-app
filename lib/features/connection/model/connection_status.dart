14e788c9c6dce55d21e14f317bd293582035eb3f
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:39:49 +0800

    rebrand cp (89/220)

--- lib/features/connection/model/connection_status.dart
+++ lib/features/connection/model/connection_status.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/features/connection/model/connection_failure.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/features/connection/model/connection_failure.dart';
 
 part 'connection_status.freezed.dart';
 
