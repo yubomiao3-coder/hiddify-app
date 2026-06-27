5b6933fb0ef1df3c47faf7efc053b11233b3b375
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:22:51 +0800

    rebrand: cloud-VPN (60/220)

--- lib/core/theme/app_theme_mode.dart
+++ lib/core/theme/app_theme_mode.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hiddify/core/localization/translations.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
 
 enum AppThemeMode {
   system,
