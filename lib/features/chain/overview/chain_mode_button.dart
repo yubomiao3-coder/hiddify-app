f2b7a1bcecccdfdf583b2511ca64ebc36e72e1fe
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:37:32 +0800

    rebrand cp (78/220)

--- lib/features/chain/overview/chain_mode_button.dart
+++ lib/features/chain/overview/chain_mode_button.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/features/chain/model/chain_enum.dart';
-import 'package:hiddify/features/chain/overview/chain_mode_icon.dart';
-import 'package:hiddify/features/chain/overview/chain_mode_menu.dart';
-import 'package:hiddify/features/common/custom_text_scroll.dart';
-import 'package:hiddify/features/settings/data/config_option_repository.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/features/chain/model/chain_enum.dart';
+import 'package:cloud_vpn/features/chain/overview/chain_mode_icon.dart';
+import 'package:cloud_vpn/features/chain/overview/chain_mode_menu.dart';
+import 'package:cloud_vpn/features/common/custom_text_scroll.dart';
+import 'package:cloud_vpn/features/settings/data/config_option_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ChainModeButton extends HookConsumerWidget {
