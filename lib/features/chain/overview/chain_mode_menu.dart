18e58ebded5215b01ebe697855079b0f6609fb59
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:37:57 +0800

    rebrand cp (80/220)

--- lib/features/chain/overview/chain_mode_menu.dart
+++ lib/features/chain/overview/chain_mode_menu.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/core/widget/adaptive_menu.dart';
-import 'package:hiddify/features/chain/model/chain_enum.dart';
-import 'package:hiddify/features/chain/overview/chain_mode_icon.dart';
-import 'package:hiddify/features/settings/data/config_option_repository.dart';
-import 'package:hiddify/singbox/model/singbox_config_enum.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/core/widget/adaptive_menu.dart';
+import 'package:cloud_vpn/features/chain/model/chain_enum.dart';
+import 'package:cloud_vpn/features/chain/overview/chain_mode_icon.dart';
+import 'package:cloud_vpn/features/settings/data/config_option_repository.dart';
+import 'package:cloud_vpn/singbox/model/singbox_config_enum.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ChainModeMenu extends HookConsumerWidget {
