95ad48ca1b2f2f2fad807181a68cf0f9787d7828
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:15:47 +0800

    rebrand: cloud-VPN (26/220)

--- lib/core/preferences/preferences_provider.dart
+++ lib/core/preferences/preferences_provider.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:hiddify/core/model/environment.dart';
-import 'package:hiddify/utils/platform_utils.dart';
+import 'package:cloud_vpn/core/model/environment.dart';
+import 'package:cloud_vpn/utils/platform_utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:loggy/loggy.dart';
 import 'package:path/path.dart' as p;
