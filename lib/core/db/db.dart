f4f1954b99b1ced7e5114d8590e45f876b056886
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:11:51 +0800

    rebrand: cloud-VPN (7/220)

--- lib/core/db/db.dart
+++ lib/core/db/db.dart
@@ -1,11 +1,11 @@
 import 'package:drift/drift.dart';
 import 'package:drift_flutter/drift_flutter.dart';
-import 'package:hiddify/core/db/converters/duration_converter.dart';
-import 'package:hiddify/core/db/db.steps.dart';
-import 'package:hiddify/core/directories/directories_provider.dart';
-import 'package:hiddify/features/per_app_proxy/model/per_app_proxy_mode.dart';
-import 'package:hiddify/features/profile/model/profile_entity.dart';
-import 'package:hiddify/utils/custom_loggers.dart';
+import 'package:cloud_vpn/core/db/converters/duration_converter.dart';
+import 'package:cloud_vpn/core/db/db.steps.dart';
+import 'package:cloud_vpn/core/directories/directories_provider.dart';
+import 'package:cloud_vpn/features/per_app_proxy/model/per_app_proxy_mode.dart';
+import 'package:cloud_vpn/features/profile/model/profile_entity.dart';
+import 'package:cloud_vpn/utils/custom_loggers.dart';
 
 part 'db.g.dart';
 
