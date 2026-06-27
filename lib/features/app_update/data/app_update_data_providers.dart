f3420b861f09cae1a56f1d1f522f8dff49f61ffa
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:24:30 +0800

    rebrand: cloud-VPN (68/220)

--- lib/features/app_update/data/app_update_data_providers.dart
+++ lib/features/app_update/data/app_update_data_providers.dart
@@ -1,5 +1,5 @@
-import 'package:hiddify/core/http_client/http_client_provider.dart';
-import 'package:hiddify/features/app_update/data/app_update_repository.dart';
+import 'package:cloud_vpn/core/http_client/http_client_provider.dart';
+import 'package:cloud_vpn/features/app_update/data/app_update_repository.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'app_update_data_providers.g.dart';
