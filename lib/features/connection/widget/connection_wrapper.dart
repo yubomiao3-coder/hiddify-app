f533191e5465e9afe3b817bfc7de9d75d5641a83
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:40:13 +0800

    rebrand cp (91/220)

--- lib/features/connection/widget/connection_wrapper.dart
+++ lib/features/connection/widget/connection_wrapper.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/core/notification/in_app_notification_controller.dart';
-import 'package:hiddify/features/connection/notifier/connection_notifier.dart';
-import 'package:hiddify/features/profile/notifier/active_profile_notifier.dart';
-import 'package:hiddify/features/settings/notifier/config_option/config_option_notifier.dart';
-import 'package:hiddify/utils/custom_loggers.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/core/notification/in_app_notification_controller.dart';
+import 'package:cloud_vpn/features/connection/notifier/connection_notifier.dart';
+import 'package:cloud_vpn/features/profile/notifier/active_profile_notifier.dart';
+import 'package:cloud_vpn/features/settings/notifier/config_option/config_option_notifier.dart';
+import 'package:cloud_vpn/utils/custom_loggers.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ConnectionWrapper extends StatefulHookConsumerWidget {
