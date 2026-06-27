6750671a2de0606fe8535d18a4eb8e913b83b3e5
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:39:11 +0800

    rebrand cp (86/220)

--- lib/features/connection/data/connection_data_providers.dart
+++ lib/features/connection/data/connection_data_providers.dart
@@ -1,8 +1,8 @@
-import 'package:hiddify/core/directories/directories_provider.dart';
-import 'package:hiddify/features/connection/data/connection_repository.dart';
-import 'package:hiddify/features/profile/data/profile_data_providers.dart';
-import 'package:hiddify/features/settings/data/config_option_data_providers.dart';
-import 'package:hiddify/hiddifycore/hiddify_core_service_provider.dart';
+import 'package:cloud_vpn/core/directories/directories_provider.dart';
+import 'package:cloud_vpn/features/connection/data/connection_repository.dart';
+import 'package:cloud_vpn/features/profile/data/profile_data_providers.dart';
+import 'package:cloud_vpn/features/settings/data/config_option_data_providers.dart';
+import 'package:cloud_vpn/hiddifycore/hiddify_core_service_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
