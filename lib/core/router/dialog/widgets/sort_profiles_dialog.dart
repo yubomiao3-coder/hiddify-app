588b834e0215160cef211976c1d21d55550416cc
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:21:23 +0800

    rebrand: cloud-VPN (53/220)

--- lib/core/router/dialog/widgets/sort_profiles_dialog.dart
+++ lib/core/router/dialog/widgets/sort_profiles_dialog.dart
@@ -1,9 +1,9 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/core/model/constants.dart';
-import 'package:hiddify/features/profile/model/profile_sort_enum.dart';
-import 'package:hiddify/features/profile/overview/profiles_notifier.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/core/model/constants.dart';
+import 'package:cloud_vpn/features/profile/model/profile_sort_enum.dart';
+import 'package:cloud_vpn/features/profile/overview/profiles_notifier.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class SortProfilesDialog extends HookConsumerWidget {
