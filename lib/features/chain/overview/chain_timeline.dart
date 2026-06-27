0f2d2e5100ba1c5e2c4518a330d5e3d2bba83fb8
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:38:22 +0800

    rebrand cp (82/220)

--- lib/features/chain/overview/chain_timeline.dart
+++ lib/features/chain/overview/chain_timeline.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/features/chain/model/chain_enum.dart';
-import 'package:hiddify/features/chain/overview/chain_timeline_arrow.dart';
-import 'package:hiddify/features/chain/overview/chain_timeline_header.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/features/chain/model/chain_enum.dart';
+import 'package:cloud_vpn/features/chain/overview/chain_timeline_arrow.dart';
+import 'package:cloud_vpn/features/chain/overview/chain_timeline_header.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ChainTimeline extends HookConsumerWidget {
