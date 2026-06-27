53853ad70067db2427f33081ee1b746cb8a5c8ca
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:18:29 +0800

    rebrand: cloud-VPN (39/220)

--- lib/core/router/dialog/widgets/custom_alert_dialog.dart
+++ lib/core/router/dialog/widgets/custom_alert_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hiddify/core/localization/translations.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class CustomAlertDialog extends HookConsumerWidget {
