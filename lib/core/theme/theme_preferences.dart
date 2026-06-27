caa6d4518515a3a40378b662487ebe4b7814d5c5
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:23:03 +0800

    rebrand: cloud-VPN (61/220)

--- lib/core/theme/theme_preferences.dart
+++ lib/core/theme/theme_preferences.dart
@@ -1,5 +1,5 @@
-import 'package:hiddify/core/preferences/preferences_provider.dart';
-import 'package:hiddify/core/theme/app_theme_mode.dart';
+import 'package:cloud_vpn/core/preferences/preferences_provider.dart';
+import 'package:cloud_vpn/core/theme/app_theme_mode.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'theme_preferences.g.dart';
