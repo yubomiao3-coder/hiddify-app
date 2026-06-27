e130ddc6fb87ffdebd65cd9060ee6ff2dab9fea1
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:12:40 +0800

    rebrand: cloud-VPN (11/220)

--- lib/core/http_client/dio_http_client.dart
+++ lib/core/http_client/dio_http_client.dart
@@ -5,7 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:dio_smart_retry/dio_smart_retry.dart';
 
-import 'package:hiddify/utils/custom_loggers.dart';
+import 'package:cloud_vpn/utils/custom_loggers.dart';
 
 class DioHttpClient with InfraLogger {
   final Map<String, Dio> _dio = {};
