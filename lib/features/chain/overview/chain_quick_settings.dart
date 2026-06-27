61bf3ee238c4d5b30bbebe375b7d686acf3e7038
yubomiao3-coder <yubomiao3@gmail.com>
2026-06-27 15:38:10 +0800

    rebrand cp (81/220)

--- lib/features/chain/overview/chain_quick_settings.dart
+++ lib/features/chain/overview/chain_quick_settings.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
-import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/core/model/constants.dart';
-import 'package:hiddify/features/chain/overview/chain_mode_button.dart';
-import 'package:hiddify/features/common/custom_text_scroll.dart';
-import 'package:hiddify/features/profile/notifier/active_profile_notifier.dart';
-import 'package:hiddify/features/settings/data/config_option_repository.dart';
+import 'package:cloud_vpn/core/localization/translations.dart';
+import 'package:cloud_vpn/core/model/constants.dart';
+import 'package:cloud_vpn/features/chain/overview/chain_mode_button.dart';
+import 'package:cloud_vpn/features/common/custom_text_scroll.dart';
+import 'package:cloud_vpn/features/profile/notifier/active_profile_notifier.dart';
+import 'package:cloud_vpn/features/settings/data/config_option_repository.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ChainQuickSettings extends HookConsumerWidget {
